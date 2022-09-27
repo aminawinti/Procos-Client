5197413aa8464a05cde645ed4608adddf3a332c2
Amin-Awinti <awintiamine@gmail.com>
2022-09-27 17:24:33 +0100

    fix api links

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -63,7 +63,9 @@ function ProductScreen() {
     const fetchData = async () => {
       dispatch({ type: 'FETCH_REQUEST' });
       try {
-        const result = await axios.get(`/api/products/slug/${slug}`);
+        const result = await axios.get(
+          `https://procos.herokuapp.com/api/products/slug/${slug}`
+        );
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
         dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
